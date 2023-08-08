f67ff7b573eb31ee9e480dd8ebb885097fee2bba
Ahmed Elghrbawy <ahmed.elghrbawy16@gmail.com>
2023-08-08 21:49:11 +0300

    register services

--- client/DistributedReddit.Web/Controllers/HomeController.cs
+++ client/DistributedReddit.Web/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DistributedReddit.Web.Models;
+using DistributedReddit.Services;
 
 namespace DistributedReddit.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly PostService _postService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, PostService postService)
     {
         _logger = logger;
+        _postService = postService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        ViewData["posts"] = await _postService.GetPostsAsync();
         return View();
     }
 
--- client/DistributedReddit.Web/Program.cs
+++ client/DistributedReddit.Web/Program.cs
@@ -1,7 +1,56 @@
+using System.Text.Json;
+using DistributedReddit.Services;
+using RDB.TransactionManager;
+using rdb_grpc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string txManagerConfPath = builder.Configuration.GetRequiredSection("tx_manager_config_file_path").Get<string>()!;
+var txManagerConfig = JsonSerializer.Deserialize<TransactionManagerConfig>(File.ReadAllText(txManagerConfPath))!;
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSingleton<ITransactionManager, TransactionManager>();
+builder.Services.AddSingleton<ITransactionManagerConfig>(txManagerConfig);
+
+builder.Services.AddTransient<SubredditService>();
+builder.Services.AddTransient<PostService>();
+builder.Services.AddTransient<UserService>();
+builder.Services.AddTransient<CommentService>();
+
+builder.Services.AddTransient<SubredditTransactionManager>();
+builder.Services.AddTransient<PostTransactionManager>();
+builder.Services.AddTransient<UserTransactionManager>();
+builder.Services.AddTransient<CommentTransactionManager>();
+
+for (int i = 0; i < txManagerConfig.NumberOfShards; i++)
+{
+    for (int j = 0; j < txManagerConfig.NumberOfReplicas; j++)
+    {
+        // ! make sure not to pass any interpolated string to options.Address. 
+        // ! It seems like options is lazily evaluated (maybe because it is an Action) so it might cause a problem.
+        // ! define a variable that is immediately evaluted and pass it to options.  
+        var channelAddres = new Uri($"http://{txManagerConfig.ShardRdbServersIps[i][j]}");
+
+        builder.Services.AddGrpcClient<SubredditGRPC.SubredditGRPCClient>($"{nameof(SubredditGRPC.SubredditGRPCClient)}/S{i}_R{j}", o => {
+            o.Address = channelAddres;
+        });
+        builder.Services.AddGrpcClient<UserGRPC.UserGRPCClient>($"{nameof(UserGRPC.UserGRPCClient)}/S{i}_R{j}", o => {
+            o.Address = channelAddres;
+        });
+        builder.Services.AddGrpcClient<PostGRPC.PostGRPCClient>($"{nameof(PostGRPC.PostGRPCClient)}/S{i}_R{j}", o => {
+            o.Address = channelAddres;
+        });
+        builder.Services.AddGrpcClient<CommentGRPC.CommentGRPCClient>($"{nameof(CommentGRPC.CommentGRPCClient)}/S{i}_R{j}", o => {
+            o.Address = channelAddres;
+        });
+        builder.Services.AddGrpcClient<TwoPhaseCommitGRPC.TwoPhaseCommitGRPCClient>($"{nameof(TwoPhaseCommitGRPC.TwoPhaseCommitGRPCClient)}/S{i}_R{j}", o => {
+            o.Address = channelAddres;
+        });
+    }
+}
 
 var app = builder.Build();
 

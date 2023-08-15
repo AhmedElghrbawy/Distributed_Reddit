72ffa06621417f4c1372b34d813a1b0825c3ecb3
Ahmed Elghrbawy <ahmed.elghrbawy16@gmail.com>
2023-08-15 19:43:29 +0300

    UI: leave join subreddit

--- client/DistributedReddit.Web/Pages/Subreddits/Index.cshtml.cs
+++ client/DistributedReddit.Web/Pages/Subreddits/Index.cshtml.cs
@@ -17,27 +17,61 @@ public class IndexModel : PageModel
 {
     private readonly UserManager<AuthUser> _userManager;
     private readonly SubredditService _subredditService;
+    private readonly UserService _userService;
+
     public IndexModel(
             UserManager<AuthUser> userManager,
-            SubredditService subredditService
+            SubredditService subredditService,
+            UserService userService
         )
     {
         _userManager = userManager;
         _subredditService = subredditService;
+        _userService = userService;
     }
 
-
+    [BindProperty]
     public Subreddit Subreddit { get; set; }
+    public User RdbUser { get; set; }
 
 
 
     public async Task<IActionResult> OnGetAsync(string handle)
     {
         Subreddit = await _subredditService.GetSubredditAsync(handle);
+        var authUser = await _userManager.GetUserAsync(User);
+        RdbUser = await _userService.GetUserAsync(authUser.Handle);
 
         if (Subreddit == null)
             return NotFound();
 
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostLeaveAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var authUser = await _userManager.GetUserAsync(User);
+        
+        await _userService.LeaveSubredditAsync(authUser.Handle, Subreddit.Handle);
+
+        return RedirectToPage("Index");
+    }
+    public async Task<IActionResult> OnPostJoinAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var authUser = await _userManager.GetUserAsync(User);
+        
+        await _userService.JoinSubredditAsync(authUser.Handle, Subreddit.Handle);
+
+        return RedirectToPage("Index");
     }
 }

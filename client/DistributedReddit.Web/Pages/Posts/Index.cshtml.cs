bc99373da6973055f01585fefa761ae74acf09ed
Ahmed Elghrbawy <ahmed.elghrbawy16@gmail.com>
2023-08-19 03:34:14 +0300

    UI: add comment

--- client/DistributedReddit.Web/Pages/Posts/Index.cshtml.cs
+++ client/DistributedReddit.Web/Pages/Posts/Index.cshtml.cs
@@ -19,25 +19,46 @@ public class IndexModel : PageModel
     private readonly UserManager<AuthUser> _userManager;
     private readonly PostService _postService;
     private readonly UserService _userService;
+    private readonly CommentService _commentService;
     public IndexModel(
             UserManager<AuthUser> userManager,
             PostService postService,
-            UserService userService
+            UserService userService,
+            CommentService commentService
         )
     {
         _userManager = userManager;
         _postService = postService;
         _userService = userService;
+        _commentService = commentService;
     }
+    [BindProperty]
     public Post Post { get; set; }
+
     [BindProperty(SupportsGet = true)]
     public string SubredditHandle {get; set;}
     
 
+    [BindProperty]
+    public Comment AddedComment { get; set; }
     public  async Task<IActionResult> OnGetAsync(string id)
     {
+        System.Console.WriteLine(id);
         Post = await _postService.GetPostAsync(Guid.Parse(id), SubredditHandle);
         return Page();
+    }
+
+    public async Task<IActionResult> OnPostAddCommentAsync()
+    {
+        var authUser = await _userManager.GetUserAsync(User);
+        
+        AddedComment.PostId = Post.Id;
+        AddedComment.Id = Guid.NewGuid().ToString();
+        AddedComment.OwnerHandle = authUser.Handle;
+        
+        await _commentService.CreateCommentAsync(AddedComment, Post.Subreddit.Handle);
+        System.Console.WriteLine("Redirected id" + Post.Id);
+        return RedirectToPage("./Index", new {id = Post.Id, SubredditHandle = Post.Subreddit.Handle});
     }
 
 }
